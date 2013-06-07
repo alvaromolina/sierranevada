4addafc9a7c8957459e4b3a435c07600545ceab8
Miguel Alvaro Molina <alvaromolinac@gmail.com>
2013-06-07 00:23:54 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sierranevada::Application.config.secret_token = '5b5bec28381e7c780fd962ed2096e05da2e1174c83a6f6c90fc33c78e81b7702e4c9c7e78baf208aa489f1b467517de109c902f358d20afa95532573f1338a09'
